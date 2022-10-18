3cd7cabb318dd1fd7ae3753fe8f62e9b2bd88772
EVOKE <evoke.lektrique@gmail.com>
2022-10-18 20:21:04 +0330

    Add rounded corners

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,8 +1,16 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
-// var icon = require('@fluentui/svg-icons/icons/add_20_filled.svg');
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// Smooth corners
+CSS.paintWorklet.addModule(
+    `data:application/javascript;charset=utf8,${encodeURIComponent(`
+registerPaint("smooth-corners",class{static get inputProperties(){return["--smooth-corners"]}superellipse(t,e,s=4,r){Number.isNaN(s)&&(s=4),(void 0===r||Number.isNaN(r))&&(r=s),s>100&&(s=100),r>100&&(r=100),s<1e-11&&(s=1e-11),r<1e-11&&(r=1e-11);const o=2/s,a=r?2/r:o,i=2*Math.PI/360;return Array.from({length:360},(s,r)=>(s=>{const r=Math.cos(s),i=Math.sin(s);return{x:Math.abs(r)**o*t*Math.sign(r),y:Math.abs(i)**a*e*Math.sign(i)}})(r*i))}paint(t,e,s){const[r,o]=s.get("--smooth-corners").toString().replace(/ /g,"").split(","),a=e.width/2,i=e.height/2,n=this.superellipse(a,i,parseFloat(r,10),parseFloat(o,10));t.fillStyle="#000",t.setTransform(1,0,0,1,a,i),t.beginPath();for(let e=0;e<n.length;e++){const{x:s,y:r}=n[e];0===e?t.moveTo(s,r):t.lineTo(s,r)}t.closePath(),t.fill()}});
+`)}`
+);
+
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
