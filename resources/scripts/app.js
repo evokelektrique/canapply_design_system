90d3b71555028f249ea5dbe2652beaf0d9a74f23
EVOKE <evoke.lektrique@gmail.com>
2022-11-22 15:22:36 +0330

    Fix modals and Add callable modal feature

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -4,11 +4,108 @@ import NiceSelect from "nice-select2/dist/js/nice-select2";
 import intlTelInput from "intl-tel-input";
 window.intlTelInput = intlTelInput;
 window.bootstrap = bootstrap;
+window.ca = {}; // Global CanApply functions
+
+async function modal_request(url, method, payload) {
+    const options =
+        method === "GET" && payload === ""
+            ? {}
+            : {
+                  method: method,
+                  body: payload,
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+              };
+    const req = await fetch(url, options);
+
+    if (req.status !== 200) {
+        alert("Server not responding");
+    }
+
+    const res = await req.text();
+
+    return res;
+}
+
+function toggle_modal(modal) {
+    modal.classList.toggle("show");
+    document.querySelector(".modal-backdrop").classList.toggle("show");
+}
+
+window.ca.display_normal_modal = (options) => {
+    const default_icon_class = "modal-icon fs-xl-3 d-flex ";
+    const type = options["data-modal-type"];
+    const title = options["data-modal-title"];
+    const body = options["data-modal-body"];
+    const icon = options["data-modal-icon"];
+    const hide_icon = options["data-modal-hidden-icon"];
+    const button_text = options["data-modal-button-text"];
+    const button_dismiss_text = options["data-modal-button-dismiss-text"];
+    const href = options["data-modal-href"];
+    const method = options["data-modal-method"];
+    const payload = options["data-modal-payload"];
+
+    const modal = document.getElementById(type + "_modal");
+    const modal_title = modal.querySelector(".modal-title");
+    const modal_body = modal.querySelector(".modal-body .message");
+    const modal_icon_wrapper = modal.querySelector(".modal-icon-wrapper");
+    const modal_icon = modal.querySelector(".modal-icon");
+    const modal_ok_button = modal.querySelector(".button-ok");
+    const modal_dismiss_button = modal.querySelector(".button-dismiss");
+
+    // Create an instance from modal, and display it
+    const modal_instance = new bootstrap.Modal(modal, {})
+    modal_instance.show();
+
+    if (type === "confirm") {
+        modal_ok_button.addEventListener(
+            "click",
+            async (e) => {
+                e.preventDefault();
+                await modal_request(href, method, payload);
+                toggle_modal(modal);
+            },
+            { once: true }
+        );
+    }
+
+    if (button_dismiss_text !== null) {
+        modal_dismiss_button.innerHTML = button_dismiss_text;
+    }
+
+    if (button_text !== null) {
+        modal_ok_button.innerHTML = button_text;
+    }
+
+    // Hide Icon
+    if (hide_icon !== null) {
+        modal_icon_wrapper.classList.add("d-none");
+    }
+
+    // Change Icon
+    if (icon !== null) {
+        const icon_class = default_icon_class + icon;
+        modal_icon.classList = icon_class;
+    }
+
+    // Change Title
+    modal_title.innerHTML = title;
+
+    // Change Body
+    modal_body.innerHTML = body;
+}
 
 function normal_modal(event, modal) {
+    // Prevent errors while displaying modals via JavaScript
+    if(event.relatedTarget === undefined) {
+        return;
+    }
+
     const default_icon_class = "modal-icon fs-xl-3 d-flex ";
     const button = event.relatedTarget;
 
+    const type = button.getAttribute("data-modal-type");
     const title = button.getAttribute("data-modal-title");
     const body = button.getAttribute("data-modal-body");
     const icon = button.getAttribute("data-modal-icon");
@@ -17,8 +114,9 @@ function normal_modal(event, modal) {
     const button_dismiss_text = button.getAttribute(
         "data-modal-button-dismiss-text"
     );
-    // const href = button.getAttribute("data-href");
-
+    const href = button.getAttribute("data-modal-href");
+    const method = button.getAttribute("data-modal-method");
+    const payload = button.getAttribute("data-modal-payload");
     const modal_title = modal.querySelector(".modal-title");
     const modal_body = modal.querySelector(".modal-body .message");
     const modal_icon_wrapper = modal.querySelector(".modal-icon-wrapper");
@@ -26,9 +124,17 @@ function normal_modal(event, modal) {
     const modal_ok_button = modal.querySelector(".button-ok");
     const modal_dismiss_button = modal.querySelector(".button-dismiss");
 
-    // if(href !== null) {
-    //     fetch()
-    // }
+    if (type === "confirm") {
+        modal_ok_button.addEventListener(
+            "click",
+            async (e) => {
+                e.preventDefault();
+                await modal_request(href, method, payload);
+                toggle_modal(modal);
+            },
+            { once: true }
+        );
+    }
 
     if (button_dismiss_text !== null) {
         modal_dismiss_button.innerHTML = button_dismiss_text;
@@ -60,7 +166,13 @@ const success_modal = document.getElementById("success_modal");
 const warning_modal = document.getElementById("warning_modal");
 const error_modal = document.getElementById("error_modal");
 const info_modal = document.getElementById("info_modal");
+const confirm_modal = document.getElementById("confirm_modal");
 
+if (confirm_modal) {
+    confirm_modal.addEventListener("show.bs.modal", (event) =>
+        normal_modal(event, confirm_modal)
+    );
+}
 if (info_modal) {
     info_modal.addEventListener("show.bs.modal", (event) =>
         normal_modal(event, info_modal)
@@ -123,10 +235,10 @@ Array.from(document.querySelectorAll(".nice-select-search")).forEach(
 
 Array.from(document.querySelectorAll(".nice-select")).forEach((select) => {
     const s = new NiceSelect(select, { searchable: false });
-    const dropdown = s.dropdown.querySelector(".nice-select-dropdown")
+    const dropdown = s.dropdown.querySelector(".nice-select-dropdown");
 
     // Change select input width relative to its dropdown width
-    if(dropdown) {
+    if (dropdown) {
         const dropdown_width = dropdown.offsetWidth;
         s.dropdown.style.width = dropdown_width + "px";
         s.dropdown.style.maxWidth = "100%"; // Safety
