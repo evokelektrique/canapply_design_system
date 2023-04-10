718cfb09a1790a344706888a0c78b0dca38fd9f7
EVOKE <evoke.lektrique@gmail.com>
2023-04-10 17:33:48 +0330

    Minor changes

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -248,7 +248,10 @@ Array.from(phone_inputs).forEach((input) => {
     const default_country =
         input.dataset.country === undefined ? "CA" : input.dataset.country;
 
+    const options = input.dataset.options === undefined ? [] : JSON.parse(input.dataset.options);
+
     intlTelInput(input, {
+        ...options,
         initialCountry: default_country,
         separateDialCode: true,
     });
@@ -257,7 +260,7 @@ Array.from(phone_inputs).forEach((input) => {
         e.preventDefault();
 
         // Strip everything but 1st 10 digits
-        e.target.value = e.target.value.replace(/\D/g, "").substring(0, 10);
+        e.target.value = e.target.value.replace(/\D/g, "").substring(0, 11);
 
         // var size = e.target.value.length;
         // if (size > 0) {
@@ -280,6 +283,7 @@ Array.from(document.querySelectorAll(".nice-select, .ca-select")).forEach(
     (element) => {
         const is_search = element.classList.contains("ca-select-search");
         const placeholder = element.dataset.placeholder || undefined;
+        const dropdown_max_height = element.dataset.dropdownMaxHeight || undefined;
         const search_placeholder = element.dataset.searchPlaceholder;
         const config = {};
 
@@ -292,7 +296,15 @@ Array.from(document.querySelectorAll(".nice-select, .ca-select")).forEach(
         const s = new NiceSelect(element, config);
         const dropdown = s.dropdown.querySelector(".nice-select-dropdown");
         const select = s.el;
+        const current = s.dropdown.querySelector(".current");
+        current.classList.add("d-block", "w-100");
         const is_full_width = select.classList.contains("ca-select-fullwidth");
+
+        if(dropdown_max_height) {
+            const list = s.dropdown.querySelector('.list');
+            list.style.maxHeight = `${dropdown_max_height}px`;
+            list.style.margin = "0px";
+        }
 
         // Change select input width relative to its dropdown width
         if (is_full_width) {
@@ -303,12 +315,12 @@ Array.from(document.querySelectorAll(".nice-select, .ca-select")).forEach(
     }
 );
 
-// Smooth corners
-CSS.paintWorklet.addModule(
-    `data:application/javascript;charset=utf8,${encodeURIComponent(`
-registerPaint("smooth-corners",class{static get inputProperties(){return["--smooth-corners"]}superellipse(t,e,s=4,r){Number.isNaN(s)&&(s=4),(void 0===r||Number.isNaN(r))&&(r=s),s>100&&(s=100),r>100&&(r=100),s<1e-11&&(s=1e-11),r<1e-11&&(r=1e-11);const o=2/s,a=r?2/r:o,i=2*Math.PI/360;return Array.from({length:360},(s,r)=>(s=>{const r=Math.cos(s),i=Math.sin(s);return{x:Math.abs(r)**o*t*Math.sign(r),y:Math.abs(i)**a*e*Math.sign(i)}})(r*i))}paint(t,e,s){const[r,o]=s.get("--smooth-corners").toString().replace(/ /g,"").split(","),a=e.width/2,i=e.height/2,n=this.superellipse(a,i,parseFloat(r,10),parseFloat(o,10));t.fillStyle="#000",t.setTransform(1,0,0,1,a,i),t.beginPath();for(let e=0;e<n.length;e++){const{x:s,y:r}=n[e];0===e?t.moveTo(s,r):t.lineTo(s,r)}t.closePath(),t.fill()}});
-`)}`
-);
+// // Smooth corners
+// CSS.paintWorklet.addModule(
+//     `data:application/javascript;charset=utf8,${encodeURIComponent(`
+// registerPaint("smooth-corners",class{static get inputProperties(){return["--smooth-corners"]}superellipse(t,e,s=4,r){Number.isNaN(s)&&(s=4),(void 0===r||Number.isNaN(r))&&(r=s),s>100&&(s=100),r>100&&(r=100),s<1e-11&&(s=1e-11),r<1e-11&&(r=1e-11);const o=2/s,a=r?2/r:o,i=2*Math.PI/360;return Array.from({length:360},(s,r)=>(s=>{const r=Math.cos(s),i=Math.sin(s);return{x:Math.abs(r)**o*t*Math.sign(r),y:Math.abs(i)**a*e*Math.sign(i)}})(r*i))}paint(t,e,s){const[r,o]=s.get("--smooth-corners").toString().replace(/ /g,"").split(","),a=e.width/2,i=e.height/2,n=this.superellipse(a,i,parseFloat(r,10),parseFloat(o,10));t.fillStyle="#000",t.setTransform(1,0,0,1,a,i),t.beginPath();for(let e=0;e<n.length;e++){const{x:s,y:r}=n[e];0===e?t.moveTo(s,r):t.lineTo(s,r)}t.closePath(),t.fill()}});
+// `)}`
+// );
 
 var tooltipTriggerList = [].slice.call(
     document.querySelectorAll('[data-bs-toggle="tooltip"]')
