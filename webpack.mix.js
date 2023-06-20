06c3cf52134773682666bcaa66902de20ba56076
EVOKE <evoke.lektrique@gmail.com>
2023-06-21 00:35:24 +0330

    Ready for publish to npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require("laravel-mix");
 
 mix.setPublicPath("dist/");
 mix.setResourceRoot("../");
-mix.js("resources/scripts/app.js", "dist/js");
-mix.sass("resources/styles/app.scss", "dist/css");
+mix.js("resources/scripts/app.js", "dist/js/canapply-ds.js");
+mix.sass("resources/styles/app.scss", "dist/css/canapply-ds.css");
